f2496b2418ba33e56f14bfc611e912f0edf68e93
Benjamin Bourgeois <benjishift@gmail.com>
2020-03-30 11:44:00 +0200

    Repo created and add Makefile

---
+++ include/myftp.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myftp_2019
+** File description:
+** TODO: add description
+*/
+
+#ifndef NWP_MYFTP_2019_MYFTP_H
+#define NWP_MYFTP_2019_MYFTP_H
+
+#endif //NWP_MYFTP_2019_MYFTP_H
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myftp_2019
+** File description:
+** TODO: add description
+*/
+
+#include "myftp.h"
+
+int main(int argc, char **argv)
+{
+    return (0);
+}
