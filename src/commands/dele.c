11dd1bf42ac9e8b14203a43c8746a3e31dc54c4c
Benjamin Bourgeois <benjishift@gmail.com>
2020-04-04 09:07:50 +0200

    Update DELE command with path

--- src/commands/dele.c
+++ src/commands/dele.c
@@ -7,14 +7,33 @@
 
 #include "myftp.h"
 
+static void build_path(char *request, char *actual, char *to_add)
+{
+    bzero(request, PATH_MAX);
+    if (to_add[0] == '/') {
+        strcpy(request, to_add);
+        return;
+    }
+    if (actual[strlen(actual) - 1] != '/')
+        actual[strlen(actual)] = '/';
+    strcpy(request, actual);
+    strcat(request, to_add);
+}
+
 void dele(server_info_t *info, client_t *client, char **cmd)
 {
     (void)info;
+    char request_path[PATH_MAX];
+    char real_path[PATH_MAX];
     if (is_client_login(client) != LOGGED)
         return (add_message_client(client, E_530PL));
-    if (get_size_array(cmd) < 2)
+    if (get_size_array(cmd) < 2 || cmd[1] == NULL)
         return (add_message_client(client, E_530P));
-    if (remove(cmd[1]))
+    build_path(request_path, client->cur_dir, cmd[1]);
+    bzero(real_path, PATH_MAX);
+    if (realpath(request_path, real_path) == NULL)
+        return (add_message_client(client, E_550F));
+    if (remove(real_path))
         return (add_message_client(client, E_550));
     add_message_client(client, E_250);
 }
--- tests/test_ftp.sh
+++ tests/test_ftp.sh
@@ -241,16 +241,17 @@ test05()
 test06()
 {
   local test_name="DELE"
-  touch file_to_delete
-  mkdir folder_to_delete
+  touch /home/bourgeois/Desktop/file_to_delete
+  mkdir /home/bourgeois/Desktop/folder_to_delete
 
   local cmd1="DELE"
   local cmd2="USER $USERNAME"
   local cmd3="PASS $PASS"
-  local cmd4="DELE file_to_delete"
+  local cmd4="CWD /home/bourgeois/Desktop"
   local cmd5="DELE file_to_delete"
-  local cmd6="DELE folder_to_delete"
+  local cmd6="DELE file_to_delete"
   local cmd7="DELE folder_to_delete"
+  local cmd8="DELE folder_to_delete"
 
   launch_client $HOST $PORT
   if [[ ! $? -eq 1 ]]; then
@@ -263,9 +264,10 @@ test06()
   launch_test "$test_name" "$cmd2" 331
   launch_test "$test_name" "$cmd3" 230
   launch_test "$test_name" "$cmd4" 250
-  launch_test "$test_name" "$cmd5" 550
-  launch_test "$test_name" "$cmd6" 250
-  launch_test "$test_name" "$cmd7" 550
+  launch_test "$test_name" "$cmd5" 250
+  launch_test "$test_name" "$cmd6" 550
+  launch_test "$test_name" "$cmd7" 250
+  launch_test "$test_name" "$cmd8" 550
 
   print_succeeded "$test_name"
   return
