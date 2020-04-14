20d0e26b9cef7df92fac8e83da95353baed9e984
Benjamin Bourgeois <benjishift@gmail.com>
2020-04-14 20:19:13 +0200

    Fix STOR command

--- src/commands/stor.c
+++ src/commands/stor.c
@@ -5,20 +5,23 @@
 ** TODO: add description
 */
 
+#include <sys/stat.h>
 #include <fcntl.h>
 #include "myftp.h"
 
 static void exec_cmd_stor(client_t *client, char **cmd)
 {
-    int output_file = open(cmd[1], O_CREAT | O_WRONLY, 0664);
+    int output_file = -1;
     char str[4096 + 1];
 
-    if (chdir(client->cur_dir) == -1 || output_file == -1) {
+    if (chdir(client->cur_dir) == -1) {
+        dprintf(client->socket_data, "Failed to exec STOR command");
+        exit(0);
+    } else if ((output_file = open(cmd[1], O_CREAT | O_WRONLY, 0664)) == -1) {
         dprintf(client->socket_data, "Failed to exec STOR command");
         exit(0);
     }
     bzero(str, 4096 + 1);
-
     while (read(client->socket_data, str, 4096) != 0) {
         dprintf(output_file, "%s", str);
         bzero(str, 4096 + 1);
@@ -27,12 +30,27 @@ static void exec_cmd_stor(client_t *client, char **cmd)
     exit(0);
 }
 
-static bool is_file_on_client(char *file)
+static bool can_create_file_on_server(client_t *client, char *file)
 {
-    int fd = open(file, O_CREAT | O_WRONLY, 0664);
-    if (fd == -1)
+    char real_path[PATH_MAX];
+    bzero(real_path, PATH_MAX);
+    int fd = -1;
+    if (file[0] != '/') {
+        strcpy(real_path, client->cur_dir);
+        if (real_path[strlen(real_path) - 1] != '/')
+            real_path[strlen(real_path)] = '/';
+        strcat(real_path, file);
+    } else {
+        strcpy(real_path, file);
+    }
+    if ((fd = open(real_path, O_CREAT | O_WRONLY, 0664)) == -1)
         return (false);
     close(fd);
+    struct stat info;
+    if (stat(real_path, &info) == -1)
+        return (false);
+    if (S_ISDIR(info.st_mode))
+        return (false);
     return (true);
 }
 
@@ -46,7 +64,7 @@ void stor(server_info_t *info, client_t *client, char **cmd)
         return (add_message_client(client, E_425));
     if (get_size_array(cmd) < 2)
         return (add_message_client(client, E_501));
-    if (!is_file_on_client(cmd[1]))
+    if (!can_create_file_on_server(client, cmd[1]))
         return (add_message_client(client, E_550));
     if (handle_mode_open(client) == TCP_ERROR)
         return (add_message_client(client, E_425U));
